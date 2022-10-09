eca90a4d5d25e6eb0a45ff94b6a030deaae7da04
wilsoncoronel <wilson_coronel_benitez@hotmail.com>
2022-10-09 11:58:18 -0500

    ConfWebpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {saludar} from './js/componentes';
+import './styles.css';
+const nombre ="Ivan"
+saludar(nombre);
---
+++ src/js/componentes.js
@@ -0,0 +1,12 @@
+import '../css/components.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
+export const saludar=(nombre='sin nombre')=>{
+    console.log("Creando etiquetas h1");
+    const h1 = document.createElement("h1");
+    h1.innerText=`Hola, ${nombre}!!!!!`;
+    document.body.append(h1);
+    //img
+    // const img = document.createElement('img');
+    // img.src= webpacklogo;
+    // document.body.append(img);
+}
